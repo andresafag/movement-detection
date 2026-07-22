fe99ca5e755001beab87090670d7ebe963ca4f80
andres acosta <andresfelipeacostagarcia34@gmail.com>
2026-07-22 11:44:51 -0500

    correction

--- infrastructure/modules/s3/variables.tf
+++ infrastructure/modules/s3/variables.tf
@@ -1,7 +1,6 @@
 variable "firmware_bucket_name" {
   type        = string
   description = "The name of the firmware"
-  default     = "bucket-firmware-esp32"
 }
 
 variable "aws_lambda_permission_allow_s3_to_invoke_lambda" {
