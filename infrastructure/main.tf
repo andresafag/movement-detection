921d4aa3e27343791dee99cea43c88082cf363e6
andres acosta <andresfelipeacostagarcia34@gmail.com>
2026-07-25 10:05:36 -0500

    hello

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -109,11 +109,11 @@ module "lambda_function" {
   attach_cloudwatch_logs_policy     = false
 
   # === EL CAMBIO CRÍTICO AQUÍ ===
-  create_package = true
-  source_path    = "${path.module}/function_lambda" # Apunta directamente a la carpeta, el módulo creará el zip de forma segura
-  artifacts_dir  = "${path.root}/.terraform/lambda-artifacts"
+  create_package          = true
+  source_path             = "${path.module}/function_lambda" # Apunta directamente a la carpeta, el módulo creará el zip de forma segura
+  artifacts_dir           = "${path.root}/.terraform/lambda-artifacts"
   ignore_source_code_hash = true
-  
+
   tags = {
     Name = "sensor-movement-esp32"
   }
